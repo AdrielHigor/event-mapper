667e556a54bf9f5465feddc8db0323be55fb4e45
AdrielHigor <adrielhigor@gmail.com>
2023-04-30 11:27:06 -0300

    add coverage inspector and test to pre-commit

--- jest.config.js
+++ jest.config.js
@@ -2,5 +2,14 @@ module.exports = {
   transform: {
     "^.+\\.(js|jsx|ts|tsx)$": "babel-jest",
     "\\.(css|less)$": "jest-transform-stub"
-  }
+  },
+  collectCoverage: true,
+  coverageThreshold: {
+    global: {
+      branches: 80,
+      functions: 80,
+      lines: 80,
+      statements: -10,
+    },
+  },
 };
